0486746d3d999e8d46bccb4d9b81cba02ec46bf6
Benyamin <khaleghibenyamin@gmail.com>
2019-12-08 12:31:03 +0330

    implemented firebase utils

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,16 +3,42 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyDUDXHwi-AaawM6zwAPLL3f13rOTuls-gg",
+  authDomain: "crwn-db-df6dc.firebaseapp.com",
+  databaseURL: "https://crwn-db-df6dc.firebaseio.com",
+  projectId: "crwn-db-df6dc",
+  storageBucket: "crwn-db-df6dc.appspot.com",
+  messagingSenderId: "325503234359",
+  appId: "1:325503234359:web:237487f7dc99fe2fa26ab2",
+  measurementId: "G-BRQXXNK9QT"
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-import '../sign-in-and-sign-up/sign-in-and-sign-up.styles.scss';
-
 import SignIn from '../../component/sign-in/sign-in.component';
+import SignUp from '../../component/sign-up/sign-up.component';
+
+import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUpPage = () => (
-    <div className="sign-in-and-sign-up">
-        <SignIn />
-    </div>
-)
+  <div className='sign-in-and-sign-up'>
+    <SignIn />
+    <SignUp />
+  </div>
+);
 
 export default SignInAndSignUpPage;
